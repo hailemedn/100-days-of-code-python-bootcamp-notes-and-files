fe124196b6fbde289b96c144e1fef615077d91c8
hailemedn <hailemedhin.k@gmail.com>
2023-10-23 14:17:58 +0300

    Day6 and Day7

---
+++ Day 6/main.py
@@ -0,0 +1,32 @@
+# functions
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# calling the function
+# my_function()
+
+
+# Reborg's world
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+
+# code challenge
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# similar functions
+# for steps in range(6)
+# for steps in range(1, 7)
+
+
+# while loops
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    number_of_hurdles -= 1
+    print(number_of_hurdles)
+
+
+
+# for loop are great when u want to do something with the items being iterated
+# while loop are great for performing a certain task many many times until a condition is switched.
+# while loop will continue until a condition is switched or met. if not it will be an infinite loop.
