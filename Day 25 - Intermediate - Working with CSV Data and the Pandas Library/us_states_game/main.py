d90bbd57719025c31c324d2ff91069d81a571cf0
hailemedn <hailemedhin.k@gmail.com>
2023-11-24 02:50:27 +0300

    from Day 26 - simplifying code using list comprehension

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/us_states_game/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/us_states_game/main.py
@@ -17,10 +17,12 @@ while len(correct_answers) < 50:
     answer_state = screen.textinput(title=f"{len(correct_answers)}/50 States Correct",
                                     prompt="what's another state name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in correct_answers:
-                missing_states.append(state)
+        # from day 26 - simplifying code using list comprehension
+        missing_states = [state for state in states if state not in correct_answers]
+        # missing_states = []
+        # for state in states:
+        #     if state not in correct_answers:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
